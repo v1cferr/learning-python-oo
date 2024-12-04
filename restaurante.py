c899c04dbadd9d806fe83592fdede5af53965796
v1cferr <dev.victorferreira@gmail.com>
2024-12-04 17:25:56 -0300

    inicio do exercicio 1.7 alura

--- restaurante.py
+++ restaurante.py
@@ -7,22 +7,26 @@ class Restaurante:
     ativo = False
 
 # Instanciando a classe Restaurante em um objeto
-restaurante_1 = Restaurante()
+restaurante_praca = Restaurante()
 
 # Atribuindo valores ao objeto
-restaurante_1.nome = "Potato"
-restaurante_1.categoria = "Comida"
-restaurante_1.ativo = True
+restaurante_praca.nome = "Praca"
+restaurante_praca.categoria = "Comida Brasileira"
+restaurante_praca.ativo = True
 
-restaurante_2 = Restaurante()
+restaurante_pizza = Restaurante()
 
-array_restaurantes = [restaurante_1, restaurante_2]
+array_restaurantes = [restaurante_praca, restaurante_pizza]
 
 # print(array_restaurantes) # Saída: apenas referencia o objeto na memória com endereços hexadecimais
 
-print(vars(restaurante_1)) # Saída: {'nome': 'teste', 'categoria': 'teste', 'ativo': 0}
+# print(vars(restaurante_praca)) # Saída: {'nome': 'Praca', 'categoria': 'Comida Brasileira', 'ativo': True}
 
 # ------------------------------------------------------------
+
+# 1- Criando uma classe
+# 2- Instanciando o objeto
+# 3- Adicionando valores aos atributos
 
 class Musica:
     nome = ""
@@ -32,4 +36,15 @@ class Musica:
 musica_1 = Musica()
 musica_1.nome = "Breezeblocks"
 musica_1.artista = "Alt-J"
-musica_1.duracao = 227000
+musica_1.duracao = 227
+
+# print(f'Musica: {musica_1.nome} - Artista: {musica_1.artista} - Duração: {musica_1.duracao} segundos')
+
+# ------------------------------------------------------------
+
+# Exercicio: https://cursos.alura.com.br/course/python-aplicando-orientacao-objetos/task/147014
+
+restaurante_praca.categoria = "Italiana"
+print(restaurante_praca.categoria)
+
+
