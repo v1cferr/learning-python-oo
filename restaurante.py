d90b2ad2f0ab24fa0eb08a37c37285e1a7892f37
v1cferr <dev.victorferreira@gmail.com>
2024-12-04 17:44:36 -0300

    add comments

--- restaurante.py
+++ restaurante.py
@@ -42,9 +42,26 @@ musica_1.duracao = 227
 
 # ------------------------------------------------------------
 
-# Exercicio: https://cursos.alura.com.br/course/python-aplicando-orientacao-objetos/task/147014
+# https://cursos.alura.com.br/course/python-aplicando-orientacao-objetos/task/147014
+
+# Em uma carreira de desenvolvimento de software, a prática consistente desempenha um papel fundamental na construção de bases sólidas. Pensando nisso, criamos uma lista de atividades (não obrigatórias) focada em prática para melhorar ainda mais sua experiência de aprendizagem. Bora praticar então?
+
+# Exercícios
+
+#     Atribua o valor 'Italiana' ao atributo categoria da instância restaurante_praca da classe Restaurante.
+#     Acesse o valor do atributo nome da instância restaurante_praca da classe Restaurante.
+#     Verifique o valor inicial do atributo ativo para a instância restaurante_praca e exiba uma mensagem informando se o restaurante está ativo ou inativo.
+#     Acesse o valor do atributo de classe categoria diretamente da classe Restaurante e armazene em uma variável chamada categoria.
+#     Altere o valor do atributo nome para 'Bistrô'.
+#     Crie uma nova instância da classe Restaurante chamada restaurante_pizza com o nome 'Pizza Place' e categoria 'Fast Food'.
+#     Verifique se a categoria da instância restaurante_pizza é 'Fast Food'.
+#     Mude o estado da instância restaurante_pizza para ativo.
+#     Imprima no console o nome e a categoria da instância restaurante_praca.
+
+# As opções de solução das atividades estão disponíveis na seção “Opinião da pessoa instrutora”.
 
 restaurante_praca.categoria = "Italiana"
 print(restaurante_praca.categoria)
+
 
 
